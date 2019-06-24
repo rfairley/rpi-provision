6454058807add237f610604fa34d59c7a7ae3ece
Robert Fairley <rob.t.fairley@gmail.com>
2019-06-23 23:42:37 -0400

    initial commit

---
+++ provision.py
@@ -0,0 +1,18 @@
+from flask import Flask, make_response, url_for, request
+app = Flask(__name__)
+
+@app.route("/")
+def entry():
+    return make_response("Request provisioning for a host by sending a POST to {}")#.format(url_for("v0/checkin")))
+
+# TODO: require authentication of the POST request
+@app.route("/v0/checkin/", methods=["GET", "POST"])
+def checkin():
+    if request.method == 'POST':
+        data = request.get_json()
+        return make_response("Host at {} has checked in, starting provisioning for user {} on port {}...\n".format(request.remote_addr, data["ssh_user"], data["ssh_port"]))
+    else:
+        return make_response("Send a POST request to check in a host for provisioning.\n")
+
+if __name__ == "__main__":
+    app.run(host="192.168.2.136", port=5000)
