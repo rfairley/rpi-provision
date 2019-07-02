15195f2a215089de4d26d7511d28f0970fb8d9a7
Robert Fairley <rob.t.fairley@gmail.com>
2019-07-01 20:03:35 -0400

    provision.py: source ipaddr and port through env vars

--- provision.py
+++ provision.py
@@ -1,6 +1,10 @@
 from flask import Flask, make_response, url_for, request
 app = Flask(__name__)
 
+import os
+host_ipaddr=os.environ["RPI_PROVISION_IPADDR"]
+host_port=int(os.environ["RPI_PROVISION_PORT"])
+
 @app.route("/")
 def entry():
     return make_response("Request provisioning for a host by sending a POST to {}")#.format(url_for("v0/checkin")))
@@ -15,4 +19,4 @@ def checkin():
         return make_response("Send a POST request to check in a host for provisioning.\n")
 
 if __name__ == "__main__":
-    app.run(host="192.168.2.136", port=5000)
+    app.run(host=host_ipaddr, port=host_port)
